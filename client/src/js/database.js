f42e42579c38072b503b71281bedf805891d4d45
Miles Thomas <milesman2013@gmail.com>
2023-01-04 16:38:00 +1100

    getDB added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Adding new content to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.add({ content });
+
+  const result = await request;
+  console.log('New data has been successfully saved', result);
+}
+
+export const getDb = async () => {
+  console.log('Retrieving content from the database');
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('Data has been retieved successfully', result);
+
+};
 
 initdb();
